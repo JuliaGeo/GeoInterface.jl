706a79f3cd45407f4f1d6f6f9db16704a197a582
Martijn Visser <mgvisser@gmail.com>
2017-04-30 13:43:05 +0200

    some more x/y/z to xcoord/ycoord/zcoord
    and adding Travis testing for 0.6 since nightly is about to diverge

--- src/GeoInterface.jl
+++ src/GeoInterface.jl
@@ -22,19 +22,19 @@ module GeoInterface
 
     abstract AbstractPosition{T <: Real} <: AbstractVector{T}
     geotype(::AbstractPosition) = :Position
-    xcoord(::AbstractPosition) = error("x(::AbstractPosition) not defined.")
-    ycoord(::AbstractPosition) = error("y(::AbstractPosition) not defined.")
+    xcoord(::AbstractPosition) = error("xcoord(::AbstractPosition) not defined.")
+    ycoord(::AbstractPosition) = error("ycoord(::AbstractPosition) not defined.")
     # optional
-    zcoord(::AbstractPosition) = error("z(::AbstractPosition) not defined.")
+    zcoord(::AbstractPosition) = error("zcoord(::AbstractPosition) not defined.")
     hasz(::AbstractPosition) = false
-    coordinates(p::AbstractPosition) = hasz(p) ? Float64[x(p),y(p),z(p)] : Float64[x(p),y(p)]
+    coordinates(p::AbstractPosition) = hasz(p) ? Float64[xcoord(p),ycoord(p),zcoord(p)] : Float64[xcoord(p),ycoord(p)]
     # (Array-like indexing # http://julia.readthedocs.org/en/latest/manual/arrays/#arrays)
     Base.eltype{T <: Real}(p::AbstractPosition{T}) = T
     Base.ndims(AbstractPosition) = 1
     Base.length(p::AbstractPosition) = hasz(p) ? 3 : 2
     Base.size(p::AbstractPosition) = (length(p),)
     Base.size(p::AbstractPosition, n::Int) = (n == 1) ? length(p) : 1
-    Base.getindex(p::AbstractPosition, i::Int) = (i==1) ? x(p) : (i==2) ? y(p) : (i==3) ? z(p) : nothing
+    Base.getindex(p::AbstractPosition, i::Int) = (i==1) ? xcoord(p) : (i==2) ? ycoord(p) : (i==3) ? zcoord(p) : nothing
     Base.convert(::Type{Vector{Float64}}, p::AbstractPosition) = coordinates(p)
     # Base.linearindexing{T <: AbstractPosition}(::Type{T}) = LinearFast()
 
