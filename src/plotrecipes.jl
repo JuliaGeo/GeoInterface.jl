d383af90fb7a9237ad64373dc14ae85061d593f6
Yeesian Ng <ngyeesian@gmail.com>
2017-12-31 16:38:30 -0500

    work with float64 for type stability

--- src/plotrecipes.jl
+++ src/plotrecipes.jl
@@ -2,53 +2,35 @@ shapecoords(geom::AbstractPoint) = [tuple(coordinates(geom)...)]
 RecipesBase.@recipe f(geom::AbstractPoint) = (seriestype --> :scatter; legend --> :false; shapecoords(geom))
 
 function shapecoords(geom::AbstractMultiPoint)
-    x, y = [], []
-    for (xi,yi) in coordinates(geom)
-        push!(x, xi); push!(y, yi)
-    end
-    x, y
+    coords = coordinates(geom)
+    first.(coordinates(geom)), last.(coordinates(geom))
 end
 RecipesBase.@recipe f(geom::AbstractMultiPoint) = (seriestype --> :scatter; legend --> :false; shapecoords(geom))
 
 function shapecoords(geom::AbstractLineString)
-    x, y = [], []
-    for (xi,yi) in coordinates(geom)
-        push!(x, xi); push!(y, yi)
-    end
-    x, y
+    coords = coordinates(geom)
+    first.(coordinates(geom)), last.(coordinates(geom))
 end
 RecipesBase.@recipe f(geom::AbstractLineString) = (seriestype --> :line; legend --> :false; shapecoords(geom))
 
 function shapecoords(geom::AbstractMultiLineString)
-    x, y = [], []
-    for line in coordinates(geom)
-        linex, liney = [], []
-        for (xi,yi) in line
-            push!(linex, xi); push!(liney, yi)
-        end
-        push!(x, linex); push!(y, liney)
-    end
+    x = Vector{Float64}[first.(line) for line in coordinates(geom)]
+    y = Vector{Float64}[last.(line) for line in coordinates(geom)]
     x, y
 end
 RecipesBase.@recipe f(geom::AbstractMultiLineString) = (seriestype --> :line; legend --> :false; shapecoords(geom))
 
 function shapecoords(geom::AbstractPolygon)
-    x, y = [], []
-    for (xi,yi) in first(coordinates(geom))
-        push!(x, xi); push!(y, yi)
-    end
-    x, y
+    ring = first(coordinates(geom)) # currently doesn't plot holes
+    first.(ring), last.(ring)
 end
 RecipesBase.@recipe f(geom::AbstractPolygon) = (seriestype --> :shape; legend --> :false; shapecoords(geom))
 
 function shapecoords(geom::AbstractMultiPolygon)
-    x, y = [], []
+    x, y = Vector{Float64}[], Vector{Float64}[]
     for poly in coordinates(geom)
-        ringx, ringy = [], []
-        for (xi,yi) in first(poly)
-            push!(ringx, xi); push!(ringy, yi)
-        end
-        push!(x, ringx); push!(y, ringy)
+        ring = first(coordinates(poly)) # currently doesn't plot holes
+        push!(x, first.(ring)); push!(y, last.(ring))
     end
     x, y
 end
