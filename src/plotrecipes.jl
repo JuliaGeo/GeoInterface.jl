37af05c09abbe11f7ed46909e1bd15de8bcb808f
Yeesian Ng <ngyeesian@gmail.com>
2018-01-04 13:12:23 -0500

    Add support for integration into Plots ecosystem (#12)
    * add support for integration into Plots ecosystem
    
    * work with float64 for type stability
    
    * coordinates(geom) -> coords
    
    * add vectorised versions
    
    * bugfix
    
    * switch to new syntax
    
    * add recipes for inhomogeneous collections
    
    * aspectratio := 1 and :line -> :path
    
    * apply same style for multi-geometries
    
    * bugfix
    
    * bugfix
    
    * bugfix for vector-of-multigeometries

---
+++ src/plotrecipes.jl
@@ -0,0 +1,147 @@
+shapecoords(geom::AbstractPoint) = [tuple(coordinates(geom)...)]
+RecipesBase.@recipe f(geom::AbstractPoint) = (
+    aspect_ratio := 1;
+    seriestype --> :scatter;
+    legend --> :false;
+    shapecoords(geom)
+)
+
+shapecoords(geom::AbstractVector{<:AbstractPoint}) = Tuple{Float64,Float64}[
+    tuple(coordinates(g)...) for g in geom
+]
+
+function shapecoords(geom::AbstractMultiPoint)
+    x, y = Float64[], Float64[]
+    for pt in coordinates(geom)
+        push!(x, pt[1]); push!(x, NaN)
+        push!(y, pt[2]); push!(y, NaN)
+    end
+    x, y
+end
+RecipesBase.@recipe f(geom::AbstractMultiPoint) = (
+    aspect_ratio := 1;
+    seriestype --> :scatter;
+    legend --> :false;
+    shapecoords(geom)
+)
+
+function shapecoords(geom::Vector{<:AbstractMultiPoint})
+    x = Float64[]; y = Float64[]
+    for g in geom, pt in coordinates(g)
+        push!(x, pt[1]); push!(x, NaN)
+        push!(y, pt[2]); push!(y, NaN)
+    end
+    x, y
+end
+
+function shapecoords(geom::AbstractLineString)
+    coords = coordinates(geom)
+    first.(coords), last.(coords)
+end
+RecipesBase.@recipe f(geom::AbstractLineString) = (
+    aspect_ratio := 1;
+    seriestype --> :path;
+    legend --> :false;
+    shapecoords(geom)
+)
+
+function shapecoords(geom::Vector{<:AbstractLineString})
+    x = Vector{Float64}[]; y = Vector{Float64}[]
+    for line in geom
+        coords = coordinates(geom)
+        push!(x, first.(coords)); push!(y, last.(coords))
+    end
+    x, y
+end
+
+function shapecoords(geom::AbstractMultiLineString)
+    x, y = Float64[], Float64[]
+    for line in coordinates(geom)
+        append!(x, first.(line)); push!(x, NaN)
+        append!(y, last.(line)); push!(y, NaN)
+    end
+    x, y
+end
+RecipesBase.@recipe f(geom::AbstractMultiLineString) = (
+    aspect_ratio := 1;
+    seriestype --> :path;
+    legend --> :false;
+    shapecoords(geom)
+)
+
+function shapecoords(geom::Vector{<:AbstractMultiLineString})
+    x = Float64[]; y = Float64[]
+    for g in geom, line in coordinates(g)
+        append!(x, first.(line)); push!(x, NaN)
+        append!(y, last.(line)); push!(y, NaN)
+    end
+    x, y
+end
+
+function shapecoords(geom::AbstractPolygon)
+    ring = first(coordinates(geom)) # currently doesn't plot holes
+    first.(ring), last.(ring)
+end
+RecipesBase.@recipe f(geom::AbstractPolygon) = (
+    aspect_ratio := 1;
+    seriestype --> :shape;
+    legend --> :false;
+    shapecoords(geom)
+)
+
+function shapecoords(geom::Vector{<:AbstractPolygon})
+    x = Vector{Float64}[]; y = Vector{Float64}[]
+    for g in geom
+        ring = first(coordinates(g)) # currently doesn't plot holes
+        push!(x, first.(ring)); push!(y, last.(ring))
+    end
+    x, y
+end
+
+function shapecoords(geom::AbstractMultiPolygon)
+    x, y = Float64[], Float64[]
+    for poly in coordinates(geom)
+        ring = first(coordinates(poly)) # currently doesn't plot holes
+        append!(x, first.(ring)); push!(x, NaN)
+        append!(y, last.(ring)); push!(y, NaN)
+    end
+    x, y
+end
+RecipesBase.@recipe f(geom::AbstractMultiPolygon) = (
+    aspect_ratio := 1;
+    seriestype --> :shape;
+    legend --> :false;
+    shapecoords(geom)
+)
+
+function shapecoords(geom::Vector{<:AbstractMultiPolygon})
+    x, y = Float64[], Float64[]
+    for g in geom, poly in coordinates(g)
+        ring = first(coordinates(poly)) # currently doesn't plot holes
+        append!(x, first.(ring)); push!(x, NaN)
+        append!(y, last.(ring)); push!(y, NaN)
+    end
+    x, y
+end
+
+RecipesBase.@recipe function f(geom::Vector{<:AbstractGeometry})
+    aspect_ratio := 1
+    legend --> :false
+    for g in geom
+        @series begin
+            if g isa AbstractPoint || g isa AbstractMultiPoint
+                seriestype := :scatter
+            elseif g isa AbstractLineString || g isa AbstractMultiLineString
+                seriestype := :path
+            elseif g isa AbstractPolygon || g isa AbstractMultiPolygon
+                seriestype := :shape
+            end
+            shapecoords(g)
+        end
+    end
+end
+
+RecipesBase.@recipe f(feature::AbstractFeature) = geometry(feature)
+RecipesBase.@recipe f(features::Vector{<:AbstractFeature}) = geometry.(features)
+RecipesBase.@recipe f(collection::AbstractFeatureCollection) = features(collection)
+RecipesBase.@recipe f(collection::AbstractGeometryCollection) = geometries(collection)
