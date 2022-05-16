c4fb38038207bb9d9f4428e992189623644c695a
Maarten Pronk <git@evetion.nl>
2022-05-16 15:34:33 +0200

    Clarified a coordinate isa Real.

--- src/interface.jl
+++ src/interface.jl
@@ -80,7 +80,7 @@ issimple(geom) = issimple(geomtype(geom), geom)
 """
     getcoord(geom, i) -> Number
 
-Return the `i`th coordinate for a given `geom`.
+Return the `i`th coordinate for a given `geom`. A coordinate isa `Real`.
 Note that this is only valid for individual [`AbstractPointTrait`](@ref)s.
 """
 getcoord(geom, i::Integer) = getcoord(geomtype(geom), geom, i)
