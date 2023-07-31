549e0ab01ba0439d7c6018fb4b7fc16b613998c9
rafaqz <rafaelschouten@gmail.com>
2023-08-01 01:40:16 +0200

    f

--- test/test_wrappers.jl
+++ test/test_wrappers.jl
@@ -197,11 +197,11 @@ multicurve_crs = GI.MultiCurve(multicurve; crs=EPSG(4326))
 
 
 # MultiPolygon
-multipolygon = GI.MultiPolygon(GI.Polygon([linearring, linearring]))
+polygon = GI.Polygon([linearring, linearring])
+multipolygon = GI.MultiPolygon([polygon])
 @test multipolygon == GI.MultiPolygon(multipolygon)
 @test GI.getgeom(multipolygon, 1) === polygon
 @test !GI.is3d(multipolygon)
-@show polygon
 @show polygon
 @show GI.getgeom(polygon, 1)
 # MultiPolygon extent does not infer, maybe due to nesting
