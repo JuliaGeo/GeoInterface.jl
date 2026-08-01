6881aaea6ab4af914eb9b8cdf5ed758584ba4e73
Maarten Pronk <git@evetion.nl>
2026-08-01 17:24:41 +0200

    Address comment.

--- src/wrappers.jl
+++ src/wrappers.jl
@@ -537,18 +537,16 @@ function Feature(geometry=nothing; properties=(;), crs=nothing, extent=nothing)
     end
 end
 
-_show_feature_geometry(io::IO, mime::MIME, geometry, ::Bool) =
-    show(io, mime, geometry)
-
-_show_feature_geometry(
-    io::IO, mime::MIME, geometry::WrapperGeometry, show_mz::Bool
-) = show(io, mime, geometry; show_mz)
-
-function Base.show(io::IO, ::MIME"text/plain", f::Feature; show_mz::Bool = true)
+function Base.show(io::IO, mime::MIME"text/plain", f::Feature; show_mz::Bool=true)
     compact = get(io, :compact, false)
     spacing = compact ? "" : " "
     print(io, "Feature(")
-    _show_feature_geometry(io, MIME"text/plain"(), f.parent.geometry, show_mz)
+    geom = geometry(f)
+    if geom isa WrapperGeometry
+        show(io, mime, geom; show_mz)
+    else
+        show(io, mime, geom)
+    end
     non_geom_props = filter(!=(:geometry), propertynames(f.parent))
     if !isempty(non_geom_props)
         print(io, ",$(spacing)properties$(spacing)=$(spacing)(")
